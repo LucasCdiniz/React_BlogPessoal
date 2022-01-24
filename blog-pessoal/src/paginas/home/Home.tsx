a39ce0dc791b5feb4ebb60c7993c90f8600e2a93
Lucas Diniz <lucascelestino.diniz@gmail.com>
2022-01-24 16:28:59 -0300

    Conceitos de Grid e FlexBox

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Box, Paper, Button} from '@material-ui/core';
 import './Home.css';
+import { height } from '@mui/system';
 
 function Home(){
     return(
         <>
-            <h1 className='titulo'>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className='img' />
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Titulo</h1>
+                    </Box>
+                    <img src="https://i.imgur.com/VUdtODV.jpeg" alt="Imagem Ilustrativa" style={{width: "100%", height:"100%"}}/>
+                    <Box display="flex" justifyContent="center" p={2}>
+                        <Button variant="contained" color="primary">Texto 1</Button>
+                        <Button variant="contained" color="secondary">Texto 2</Button>
+                    </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
